9f1e42494f1c7bdf6958a1a1afc0ce0098e4ed80
ief <iuriestevam@hotmail.com>
2024-08-02 20:25:14 -0300

    02082024

--- src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
+++ src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
@@ -3,8 +3,11 @@ package com.example.demo.Form.Pessoa;
 import com.example.demo.Model.Pessoa;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -13,13 +16,19 @@ public class PessoaForm {
     @NotBlank(message = "Preencha o campo nome.")
     private String nome;
 
+    @NotNull(message = "Preencha o campo data de nascimento.")
+    private LocalDate nascimento;
+
 
     public Pessoa toEntity(){
-        return new Pessoa(nome);
+        return new Pessoa(nome, nascimento);
     }
 
     public PessoaForm(Pessoa pessoa){
         this.nome = pessoa.getNome();
+        this.nascimento = pessoa.getNascimento();
     }
+
+
 
 }
--- src/main/java/com/example/demo/Model/Pessoa.java
+++ src/main/java/com/example/demo/Model/Pessoa.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @Entity
@@ -21,12 +23,17 @@ public class Pessoa {
     @Column(name = "nome", nullable = false, length = 100, unique = false)
     private String nome;
 
+    @Column(name = "nascimento", nullable = false, unique = false)
+    private LocalDate nascimento;
+
     @Column(name = "ativo", nullable = false)
     @ColumnDefault("true")
     private boolean ativo = true;
 
 
-    public Pessoa(String nome){
+    public Pessoa(String nome, LocalDate nascimento){
         this.nome = nome;
+        this.nascimento = nascimento;
     }
+
 }
