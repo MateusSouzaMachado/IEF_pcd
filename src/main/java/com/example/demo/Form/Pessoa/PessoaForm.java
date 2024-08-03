6afe10f429f38f8fe23fc8642ea7d4f99deace78
ief <iuriestevam@hotmail.com>
2024-08-02 21:45:43 -0300

    02082024

--- src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
+++ src/main/java/com/example/demo/Form/Pessoa/PessoaForm.java
@@ -1,9 +1,11 @@
 package com.example.demo.Form.Pessoa;
 
+import com.example.demo.Enum.Sexo;
 import com.example.demo.Model.Pessoa;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,11 +19,16 @@ public class PessoaForm {
     private String nome;
 
     @NotNull(message = "Preencha o campo data de nascimento.")
+    @Past(message = "Preencha o campo com uma data válida.")
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate nascimento;
-
+    
+    @NotNull(message = "Preencha o campo sexo.")
+    private int sexo;
 
     public Pessoa toEntity(){
-        return new Pessoa(nome, nascimento);
+        Sexo sexo = Sexo.fromCodigo(this.sexo);
+        return new Pessoa(nome, nascimento, sexo);
     }
 
     public PessoaForm(Pessoa pessoa){
--- src/main/java/com/example/demo/Model/Pessoa.java
+++ src/main/java/com/example/demo/Model/Pessoa.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 
+import com.example.demo.Enum.Sexo;
+
 import java.time.LocalDate;
 
 @Getter
@@ -30,10 +32,16 @@ public class Pessoa {
     @ColumnDefault("true")
     private boolean ativo = true;
 
+    @Column(name="sexo", nullable = false)
+    @ColumnDefault("2")
+    @Enumerated(EnumType.ORDINAL)
+    private Sexo sexo =  Sexo.NAO_INFORMADO;
 
-    public Pessoa(String nome, LocalDate nascimento){
+
+    public Pessoa(String nome, LocalDate nascimento, Sexo sexo) {
         this.nome = nome;
         this.nascimento = nascimento;
+        this.sexo = sexo;
     }
 
 }
