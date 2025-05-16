ee03fb1e5897de8e691cafb3e88b9cd5e4e6053c
Mateus Souza Machado <mateus2005machado@gmail.com>
2025-05-15 21:51:45 -0300

    1

--- src/test/java/com/example/demo/DeficienciaTest.java
+++ src/test/java/com/example/demo/DeficienciaTest.java
@@ -9,10 +9,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.demo.Model.Deficiencia;
+import com.example.demo.Repository.DeficienciaRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 
 
@@ -22,6 +30,9 @@ public class DeficienciaTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private DeficienciaRepository dr;
 
     @Test
     @DisplayName("Verifica se o endpont /deficiencia responde 200 (OK)")
@@ -41,6 +52,58 @@ public class DeficienciaTest {
             .param("nome", "Teste" + timestamp)
             .param("categoria", "1")
         ).andExpect(status().is3xxRedirection());
+
+        List<Deficiencia> deficiencias = dr.findAll();
+        assertNotNull(deficiencias);
+
+        Deficiencia deficiencia = deficiencias.get(deficiencias.size() - 1);
+        assertEquals(deficiencia.getNome(), "Teste" + timestamp);
+        assertEquals(deficiencia.getCategoria().getId(), 1L);
+
+        mockMvc.perform(
+            post("/deficiencia/update/" + deficiencia.getId())
+            .param("nome", "TESTE ALTERAÇÃO" + timestamp)
+            .param("categoria", "2")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        ).andExpect(status().is3xxRedirection());
+
+
+        deficiencia = dr.findById(deficiencia.getId()).orElseThrow();
+
+        assertNotNull(deficiencia);
+
+        assertEquals(deficiencia.getNome(), "TESTE ALTERAÇÃO" + timestamp);
+        assertEquals(deficiencia.getCategoria().getId(), 2L);
     }
+
+    @Test
+    @DisplayName("Remove e recupera um registro")
+    void removeAndRecovery() throws Exception{
+        List<Deficiencia> listaDeficiencias = dr.findByAtivo(true);
+
+        Deficiencia deficiencia = listaDeficiencias.get(listaDeficiencias.size() - 1);
+
+        Long id = deficiencia.getId();
+
+        mockMvc.perform(
+            get("/deficiencia/remover/" + id)
+        ).andExpect(status().is3xxRedirection());
+
+        deficiencia = dr.findById(id).orElseThrow();
+
+        assertNotNull(deficiencia);
+        
+        assertFalse(deficiencia.isAtivo());
+
+        mockMvc.perform(
+            get("/deficiencia/recuperar/" + id)
+        ).andExpect(status().is3xxRedirection());
+
+        deficiencia = dr.findById(id).orElseThrow();
+        assertNotNull(deficiencia);
+        assertTrue(deficiencia.isAtivo());
+    }
+
+    
 
 }
